5454e7d23a5bcb60e2e35b0c552be688c762662a
Albina Salkayeva <salkaeva.a@gmail.com>
2023-06-24 14:41:44 +0600

    Add logic to calculate average result from JSON data

--- src/components/ResultPanel.js
+++ src/components/ResultPanel.js
@@ -1,11 +1,11 @@
 import './ResultPanel.scss';
 import ResultScore from './ResultScore';
 
-function ResultPanel() {
+function ResultPanel({ data }) {
   return (
     <section className='result-panel'>
       <h1 className='heading-1'>Your Result</h1>
-      <ResultScore />
+      <ResultScore data={data} />
       <h2 className='heading-2'>Great</h2>
       <p className='result-panel__text'>
         You scored higher than 65% of the people who have taken these tests.
--- src/components/ResultScore.js
+++ src/components/ResultScore.js
@@ -1,9 +1,12 @@
 import './ResultScore.scss';
 
-function ResultScore() {
+function ResultScore({ data }) {
+  const resultSum = data.reduce((sum, value) => sum + value.score, 0);
+  const resultAvg = Math.round(resultSum / data.length);
+
   return (
     <div className='result-score'>
-      <p className='result-score__your-score'>76</p>
+      <p className='result-score__your-score'>{resultAvg}</p>
       <p className='result-score__total-score'>of 100</p>
     </div>
   );
