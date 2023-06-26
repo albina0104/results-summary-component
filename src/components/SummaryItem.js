8b8a5ad31d43ca0ce22ff45572b4deeee0bd5166
Albina Salkayeva <salkaeva.a@gmail.com>
2023-06-26 21:58:25 +0600

    Add relative size for summary icons

--- src/components/SummaryItem.js
+++ src/components/SummaryItem.js
@@ -3,7 +3,7 @@ import './SummaryItem.scss';
 function SummaryItem({ color, itemData }) {
   return (
     <div className={`${color} summary-item`}>
-      <img src={itemData.icon} alt='' />
+      <img className='summary-item__icon' src={itemData.icon} alt='' />
       <div className='item-type'>{itemData.category}</div>
       <div>
         <span className='score'>{itemData.score}</span>
