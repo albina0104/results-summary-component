11b8fdc390f4d60611e131f42688736235756f12
Albina Salkayeva <salkaeva.a@gmail.com>
2023-06-26 23:52:38 +0600

    Add attribution

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.scss';
 import ResultPanel from './components/ResultPanel';
 import SummaryPanel from './components/SummaryPanel';
+import Footer from './components/Footer';
 
 function App() {
   const data = require('./data/data.json');
   return (
-    <main className='App'>
-      <ResultPanel data={data} />
-      <SummaryPanel data={data} />
-    </main>
+    <>
+      <main className='App'>
+        <ResultPanel data={data} />
+        <SummaryPanel data={data} />
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer class='attribution'>
+      <p>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          href='https://github.com/albina0104'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Albina Salkayeva
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
